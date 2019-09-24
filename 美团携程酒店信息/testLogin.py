470b74676c93488dad96d54527da3668cdef6649
chara <41978235+okurumio@users.noreply.github.com>
2019-09-24 17:12:24 +0800

    Update testLogin.py

--- 美团携程酒店信息/testLogin.py
+++ 美团携程酒店信息/testLogin.py
@@ -1,39 +1,4 @@
 
-# import requests
-# url = 'https://passport.ctrip.com/gateway/api/soa2/11448/checkPhoneCode.json'
-# json = {"AccountHead":{},
-#         "Data":
-#             {"messageCode":"S200078",
-#              "code":"114986",
-#              "countryCode":"86",
-#              "mobilePhone":"13613575930"},
-#         "head":
-#             {}}
-# headers = {
-#     'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/16E227 NebulaSDK/1.8.100112 Nebula WK PSDType(0) AlipayDef',
-#     'Referer': 'https://2017081708237081.hybrid.alipay-eco.com/2017081708237081/0.2.1904232103.32/index.html#pages/accounts/login?__appxPageId=110&__navigator=6'
-# }
-# r = requests.post(url,json=json,headers=headers)
-# print(r.text)
-
-# DA60D63B05CB13838E319647B8C057A9058266D0A83800B91E31AC867474534F
-
-# url = 'https://passport.ctrip.com/gateway/api/soa2/12559/userLogin.json'
-# json1 = {"AccountHead":{},
-#         "Data":
-#             {"accessCode":"B6CE8D84FEBBFC0E",
-#              "strategyCode":"85D4BB47E79522CA",
-#              "loginName":"",
-#              "certificateCode":"DA60D63B05CB13838E319647B8C057A9058266D0A83800B91E31AC867474534F"},
-#         "head":{}}
-# headers = {
-#     'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/16E227 NebulaSDK/1.8.100112 Nebula WK PSDType(0) AlipayDef',
-#     'Referer': 'https://2017081708237081.hybrid.alipay-eco.com/2017081708237081/0.2.1904232103.32/index.html#pages/accounts/login?__appxPageId=110&__navigator=6'
-# }
-# r = requests.post(url, json=json1, headers=headers)
-# print(r.text)
-# F2BF55DA779037C874B72EB2AE7F6DB59C0416EFFB87706D378538E83BB2244F
-# F2BF55DA779037C874B72EB2AE7F6DB5DFA17E274740D2603A83474D7F78542D
 import requests
 import json
 
@@ -99,12 +64,9 @@ def login(token):
 
 
 if __name__ == '__main__':
-    # tel = '13520197084'
-    # sendCode(tel)
-    # code = input("请输入验证码:")
-    # token = checkCode(code, tel)
-    # print(token)
-    auth = login('725296CCA65F8334ED263F93FC6D842C01C110483DCD61C3669032B3314F88AE')
+    tel = '123'  # 手机号
+    sendCode(tel)
+    code = input("请输入验证码:")
+    token = checkCode(code, tel)
+    auth = login(token)
     print(auth)
-
-# 725296CCA65F8334ED263F93FC6D842C01C110483DCD61C3669032B3314F88AE
